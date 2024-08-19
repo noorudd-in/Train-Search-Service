92c3bc902cc83cc5695eb895cf137c51d44c1d19
Noor <nooruddin12401@gmail.com>
2024-08-19 19:17:46 +0530

    Adding more data, Showing departure and arrival time while searching for a train

--- src/seeders/20240810101606-add-stations.js
+++ src/seeders/20240810101606-add-stations.js
@@ -44,6 +44,150 @@ module.exports = {
           code: "HYB",
           createdAt: new Date(),
           updatedAt: new Date(),
+        },
+        {
+          name: "Nashik Road",
+          code: "NK",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Jalgaon Jn",
+          code: "JL",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Bhusaval Jn",
+          code: "BSL",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Bhopal Jn",
+          code: "BPL",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Virangana Lakshmibai Jhansi",
+          code: "VGLJ",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Gwalior",
+          code: "GWL",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Hazrat Nizamuddin (New Delhi)",
+          code: "NZM",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "New Delhi",
+          code: "NDLS",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Bhatinda Jn",
+          code: "BTI",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Firozpur Cant",
+          code: "FZR",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Secunderabad Jn",
+          code: "SC",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Warangal",
+          code: "WL",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Vijayawada Jn",
+          code: "BZA",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Rajahmundry",
+          code: "RJY",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Visakhapatnam",
+          code: "VSKP",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Srikakulam Road",
+          code: "CHE",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Brahmapur",
+          code: "BAM",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Bhubaneswar",
+          code: "BBS",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Mumbai Central",
+          code: "MMCT",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Surat",
+          code: "ST",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Bharuch Jn",
+          code: "BH",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Vadodara Jn",
+          code: "BRC",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Ratlam Jn",
+          code: "RTM",
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: "Kota Jn",
+          code: "KOTA",
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
